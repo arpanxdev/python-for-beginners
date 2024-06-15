36a91db75b8437cc93dbd80ec35af708aecb75a3
arpanxdev <felixplayz1337@gmail.com>
2024-06-15 12:22:03 +0530

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
    
     Initial commit
    
     Changes to be committed:
    	new file:   strings/modify.py
    	new file:   strings/slicing.py
    	new file:   variables/assign_multiple_values.py
    	new file:   variables/global_vars.py

---
+++ strings/modify.py
@@ -0,0 +1,11 @@
+a = "Hello, World!"
+print(a.upper())
+print(a.lower())
+print(a.strip()) # returns "Hello, World!"
+print(a.replace("H", "J"))
+print(a.split(",")) # returns ['Hello', ' World!']
+print(a.casefold())
+print(a.center(100))
+print(a.count("o"))
+print(a.encode())
+print(a.endswith('!'))
